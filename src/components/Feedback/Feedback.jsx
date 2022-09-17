e4b84cbc436fc1a8ba4c9f8558d184482c4f9eb9
Alex Makeyev <makeyev1987@gmail.com>
2022-09-17 16:15:50 +0300

    upd

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Feedback extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Please leave a feedback</h1>
+                <button type="button"></button>
+                <button type="button"></button>
+                <button type="button"></button>
+
+                <h2>Statistics</h2>
+                <span>Good</span>
+                <span>Neutral</span>
+                <span>Bad</span>
+            </div>
+        ) 
+            
+        
+    }
+
+}
+export default Feedback
